1d895f3cb4b25dbb6fa0e90eac6062e3b63285ca
momiskiv <you@example.com>
2025-01-15 11:56:22 +0000

    Initial commit

---
+++ download_fastq.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --job-name=downloading_data
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=80G
+#SBATCH --time=08:00:00
+#SBATCH --export=NONE
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=nm471@student.le.ac.uk
+#SBATCH --account=evo-epi
+
+### Include when testing; testing passed 14/01/25
+# SBATCH --partition=devel
+
+# Final output dir
+mkdir -p fastq_mine
+
+#SRA path from config
+SRA_CACHE="/scratch/evo-epi/nm471/Project/cache/sra"
+
+### Run script in the working directory it was submitted in
+cd $SLURM_SUBMIT_DIR
+
+# Load modules; updated on 14/01/25
+module load gcc/12.3.0-yxgv2bl
+module load sratoolkit/3.0.0-5fetwpi
+
+# Download data from NCBI
+prefetch --option-file Accessions.txt 
+
+# Make SRR files into fastq
+for file in ${SRA_CACHE}/SRR* 
+do
+    base_name=$(basename "$file") # need just basename or crashes
+    fasterq-dump --split-files "$file"  -O fastq_mine 
+done
